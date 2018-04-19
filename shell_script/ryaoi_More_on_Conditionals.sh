720c333bc2be6c3184426589491dd2658d4942ec
ryaoi42 <ryaoi@student.42.fr>
2018-04-19 23:12:58 +0200

    not bad

---
+++ shell_script/ryaoi_More_on_Conditionals.sh
@@ -0,0 +1,12 @@
+read X
+read Y
+read Z
+if [[ $X == $Y && $Y == $Z ]]
+then
+    echo 'EQUILATERAL'
+elif [[ $X == $Y || $Y == $Z || $Z == $X ]]
+then
+    echo 'ISOSCELES'
+else
+    echo 'SCALENE'
+fi
